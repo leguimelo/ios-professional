9dc24f129781d81f8247f3daba191126ec3a4070
Leandro Guimarães <leandroguimaraes@MacBook-Pro-de-Leandro.local>
2025-06-04 12:56:36 -0300

    feat: Add tab bar

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+let appColor: UIColor = .systemTeal
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
@@ -14,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let loginViewController = LoginViewController()
     let onboardingConteinerViewController = OnboardingContainerViewController()
     let dummyViewController = DummyViewController()
+    let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -25,9 +28,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingConteinerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = loginViewController
+        window?.rootViewController = mainViewController
         //        window?.rootViewController = onboardingConteinerViewController
         //        window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you fell like you are back in 80s.")
+        
+//        mainViewController.selectedIndex = 2
         
         return true
     }
---
+++ Bankey/Utils/UIViewController+Utils.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController+Utils.swift
+//  Bankey
+//
+//  Created by Leandro Guimarães on 04/06/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func setStatusBar() {
+        let statusBarSize = UIApplication.shared.statusBarFrame.size // deprecated but OK
+        let frame = CGRect(origin: .zero, size: statusBarSize)
+        let statusbarView = UIView(frame: frame)
+
+        statusbarView.backgroundColor = appColor
+        view.addSubview(statusbarView)
+    }
+    
+    func setTabBarImage(imageName: String, title: String) {
+        let configuration = UIImage.SymbolConfiguration(scale: .large)
+        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
+    }
+}
