b7fa697b73d748f64a6eae73cbf473830feb7cfd
Leandro Guimarães <leandroguimaraes@MacBook-Pro-de-Leandro.local>
2025-06-05 11:34:52 -0300

    feat: Add tableview

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -28,7 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingConteinerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+//        window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
         //        window?.rootViewController = onboardingConteinerViewController
         //        window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easier to use, and has a brand new look and feel that will make you fell like you are back in 80s.")
         
